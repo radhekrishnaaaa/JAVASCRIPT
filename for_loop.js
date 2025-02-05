cad7c2139149304447f1d8148ab121bedcad2837
bhumi prajapati <r2075308@gmail.com>
2025-02-05 14:32:23 +0530

    loop

---
+++ for_loop.js
@@ -0,0 +1,30 @@
+// let a=10,i;
+
+// for(i=0;i<a;i++)
+// {
+//     console.log(i);
+// }
+
+// let a=10,i;
+
+// for(i=1;i<a;i+=2)
+// {
+//     console.log(i);
+// }
+
+// let a=1,i;
+
+// for(i=10;i>a;i--)
+// {
+//     console.log(i);
+// }
+
+// let i,j;
+// for(i=1;i<5;i++)
+// {
+//     for(j=1;j<5;j++)
+//     {
+//         console.log("*");
+//     }
+//     console.log("\n");
+// }                                 // error
---
+++ while_loop.js
@@ -0,0 +1,14 @@
+// let a=1;
+// while(a<5)
+// {
+//     console.log(a);
+//     a++;
+// }
+
+
+// let a=1;
+// while(a<20)
+// {
+//     console.log(a);
+//     a+=2;
+// }
