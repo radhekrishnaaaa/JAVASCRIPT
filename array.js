fcbe24a7ac412c2980a1efc2704c232019890c84
bhumi prajapati <r2075308@gmail.com>
2025-02-05 14:33:09 +0530

    array

---
+++ array.js
@@ -0,0 +1,29 @@
+//collection of  "Homogenious" "indexed" elements called as "array"
+
+//[]
+//index starts from "0"
+//we can access array elements with "indexes"
+// The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+
+// let a=[]
+// console.log(a)
+// console.log(typeof(a))
+
+// let a=[1,2,'three',4,"FIVE",6,7.0,8.0,'nine',"ten",true]
+// console.log(typeof(a[3])) //number
+// console.log(typeof(a[4])) //string
+// console.log(a.length)     //11
+// console.log(a[a.length-1]) //10
+// console.log(typeof(a[10]))  //boolean
+
+// let arr = [10, 20, 30, 40, 50];
+// arr[2.1]=2.1
+// arr[10]=10
+// arr['11']='11'
+// let num = '22'
+// console.log(typeof(num))
+
+
+
+//insert  -- > //push()   pachalinsert  //unshift()   agal in side insert
+//delete -- > //pop()     pachaldelete  //shift()     agal ni side delete
