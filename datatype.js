b25fcdd2b4a3f1683783dfb07cf32dd65b4cc690
bhumi prajapati <r2075308@gmail.com>
2025-02-07 16:06:24 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,30 @@
+// console.log(null==NaN);
+// console.log(null===NaN);
+
+// console.log(null==undefined);
+// console.log(null===undefined);
+
+// console.log(undefined==NaN);
+// console.log(undefined===NaN);
+
+// console.log(NaN==NaN);
+// console.log(NaN==NaN);
+
+// let a=10
+// console.log(a*'s');
+// console.log(typeof(a*'s'));
+
+// console.log(undefined==undefined);
+// console.log(undefined===undefined);
+
+// console.log(a[10]==undefined);
+// console.log(a[10]===undefined);
+
+// let a=[];
+// a.push(10)
+// a.push(11)
+// a.push(12)
+// a.push(13)
+// a.push(14)
+// a[100]=99
+// console.log(a)
